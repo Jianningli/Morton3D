4bbc3f97a90c67a058988fc1e490ce8267f2449e
Jianning Li <58332790+Jianningli@users.noreply.github.com>
2020-08-22 20:02:36 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from setuptools import setup
+
+setup(
+    name='Morton3D',
+    version='1.0',
+    author='Jianning Li',
+    author_email='jianningli.me@gmail.com',
+    maintainer='Jianning Li',
+    maintainer_email='jianningli.me@gmail.com',
+    description='Efficient 3D Morton Encoding and Decoding',
+    keywords='z-order, morton order, 3D hashing',
+    url='https://github.com/Jianningli/Morton3D',
+    py_modules=['Morton3D'],
+)
